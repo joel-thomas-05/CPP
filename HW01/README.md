df63f88ae910e8e198feb6286b0305807c5dcc1a
joel-thomas-05 <joelsamtom@gmail.com>
2026-02-20 21:10:08 -0500

    Initial commit - HW01

---
+++ HW01/README.md
@@ -0,0 +1,9 @@
+# HW 01 Problem 01
+
+Compile:
+
+g++ problem1.cpp -o P1
+
+Run:
+
+./P1
---
+++ HW01/problem1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Joel" << endl;
+	return 0;
+}
